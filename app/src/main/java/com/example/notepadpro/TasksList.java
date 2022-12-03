fe4987e8b96e273684ef13091a21723776dc5116
Antonio Romano <aromanomacias@gmail.com>
2022-12-03 12:22:41 -0600

    created task list view

---
+++ app/src/main/java/com/example/notepadpro/TasksList.java
@@ -0,0 +1,14 @@
+package com.example.notepadpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TasksList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks_list);
+    }
+}
