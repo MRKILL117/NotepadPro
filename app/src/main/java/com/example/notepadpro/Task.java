33f3f0965c74be189f92853872b95b7bf6680e91
Antonio Romano <aromanomacias@gmail.com>
2022-12-04 16:39:45 -0600

    add tasks with due date works

--- app/src/main/java/com/example/notepadpro/Task.java
+++ app/src/main/java/com/example/notepadpro/Task.java
@@ -4,10 +4,19 @@ public class Task {
     protected String title;
     protected String content;
     protected String dueDate;
+    protected String dueTime;
 
     public Task(String title, String content, String dueDate) {
         this.title = title;
         this.content = content;
         this.dueDate = dueDate;
+        this.dueTime = null;
+    }
+
+    public Task(String title, String content, String dueDate, String dueTime) {
+        this.title = title;
+        this.content = content;
+        this.dueDate = dueDate;
+        this.dueTime = dueTime;
     }
 }
--- app/src/main/java/com/example/notepadpro/TasksList.java
+++ app/src/main/java/com/example/notepadpro/TasksList.java
@@ -1,22 +1,57 @@
 package com.example.notepadpro;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class TasksList extends AppCompatActivity {
 
+    private static int OK = 200;
     private List<Task> tasks = new ArrayList<Task>();
     private RecyclerView recyclerView;
     private RecyclerView.Adapter recyclerViewAdapter;
     private RecyclerView.LayoutManager recyclerViewLayoutManager;
+
+    // To start an activity and get back data
+    ActivityResultLauncher<Intent> modalLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.i("customLog", "On activity result: " + result.getResultCode());
+                    if(result.getResultCode() == OK) {
+                        Intent i = result.getData();
+                        AddTask(new Task(
+                                i.getStringExtra("taskTitle"),
+                                i.getStringExtra("taskContent"),
+                                i.getStringExtra("taskDueDate"),
+                                i.getStringExtra("taskDueTime")
+                                )
+                        );
+                    }
+                }
+            }
+    );
 
 
     @Override
@@ -49,6 +84,20 @@ public class TasksList extends AppCompatActivity {
     }
 
     public void CreateTask(View v) {
-
+        Intent i = new Intent(this, TaskForm.class);
+        this.modalLauncher.launch(i);
     }
+
+    private void AddTask(Task newTask) {
+        this.tasks.add(0, newTask);
+        this.recyclerViewAdapter.notifyItemInserted(0);
+    }
+    private void RemoveTask(int pos) {
+        this.tasks.remove(pos);
+        this.recyclerViewAdapter.notifyItemRemoved(pos);
+    }
+    private void UpdateTask(int pos) {
+        this.recyclerViewAdapter.notifyItemChanged(pos);
+    }
+
 }
