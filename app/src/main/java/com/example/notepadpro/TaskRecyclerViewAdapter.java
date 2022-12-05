cbca3007338511289b5a418a560ba4c5c60bf803
Antonio Romano <aromanomacias@gmail.com>
2022-12-04 21:19:51 -0600

    delete and edit tasks works

--- app/src/main/java/com/example/notepadpro/TaskRecyclerViewAdapter.java
+++ app/src/main/java/com/example/notepadpro/TaskRecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.graphics.fonts.Font;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
@@ -19,16 +20,21 @@ public class TaskRecyclerViewAdapter extends RecyclerView.Adapter<TaskRecyclerVi
 
     private final List<Task> tasks;
     private final int layout;
-    private final OnItemClickListener onItemClick;
+    private final OnDeleteClickListener onDelete;
+    private final OnEditClickListener onEdit;
 
-    public interface OnItemClickListener {
-        void onItemClick(String title, String content, String dueDate, String dueTime, int position);
+    public interface OnDeleteClickListener {
+        void onDeleteClick(int position);
+    }
+    public interface OnEditClickListener {
+        void onEditClick(int position);
     }
 
-    public TaskRecyclerViewAdapter(List<Task> tasks, int layout, OnItemClickListener onItemClick) {
+    public TaskRecyclerViewAdapter(List<Task> tasks, int layout, OnDeleteClickListener onDelete, OnEditClickListener onEdit) {
         this.tasks = tasks;
         this.layout = layout;
-        this.onItemClick = onItemClick;
+        this.onDelete = onDelete;
+        this.onEdit = onEdit;
     }
 
     @NonNull
@@ -41,7 +47,7 @@ public class TaskRecyclerViewAdapter extends RecyclerView.Adapter<TaskRecyclerVi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bind(tasks.get(position).title, tasks.get(position).content, tasks.get(position).dueDate, tasks.get(position).dueTime, this.onItemClick);
+        holder.bind(tasks.get(position).title, tasks.get(position).content, tasks.get(position).dueDate, tasks.get(position).dueTime, this.onDelete, this.onEdit);
     }
 
     @Override
@@ -52,6 +58,7 @@ public class TaskRecyclerViewAdapter extends RecyclerView.Adapter<TaskRecyclerVi
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView taskTitle, taskContent, taskDueDate, taskDueTime;
+        Button taskDeleteButton, taskEditButton;
         CheckBox taskChecbox;
 
         public ViewHolder(@NonNull View itemView) {
@@ -61,9 +68,11 @@ public class TaskRecyclerViewAdapter extends RecyclerView.Adapter<TaskRecyclerVi
             this.taskDueDate = itemView.findViewById(R.id.taskDueDate);
             this.taskDueTime = itemView.findViewById(R.id.taskDueTime);
             this.taskChecbox = itemView.findViewById(R.id.taskCheckbox);
+            this.taskDeleteButton = itemView.findViewById(R.id.deleteTaskButton);
+            this.taskEditButton = itemView.findViewById(R.id.editTaskButton);
         }
 
-        public void bind(String title, String content, String dueDate, String dueTime, final TaskRecyclerViewAdapter.OnItemClickListener itemListener) {
+        public void bind(String title, String content, String dueDate, String dueTime, final TaskRecyclerViewAdapter.OnDeleteClickListener onDelete, final TaskRecyclerViewAdapter.OnEditClickListener onEdit) {
             this.taskTitle.setText(title);
             this.taskContent.setText(content);
             this.taskDueDate.setText(dueDate);
@@ -71,6 +80,7 @@ public class TaskRecyclerViewAdapter extends RecyclerView.Adapter<TaskRecyclerVi
             this.taskChecbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                    taskEditButton.setEnabled(!checked);
                     if(checked) {
                         taskTitle.setPaintFlags(taskTitle.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
                         taskContent.setPaintFlags(taskContent.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
@@ -85,10 +95,17 @@ public class TaskRecyclerViewAdapter extends RecyclerView.Adapter<TaskRecyclerVi
                     }
                 }
             });
-            itemView.setOnClickListener(new View.OnClickListener(){
+
+            this.taskDeleteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view){
-                    itemListener.onItemClick(title, content, dueDate, dueTime, getAdapterPosition());
+                public void onClick(View view) {
+                    onDelete.onDeleteClick(getAdapterPosition());
+                }
+            });
+            this.taskEditButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onEdit.onEditClick(getAdapterPosition());
                 }
             });
         }
